4439f037905b5d6a6267d6ef3e5346f33b7ea4b8
timmPrice <timmPrice6@gmail.com>
2025-01-13 14:49:18 -0500

    can load sudoku puzzles into board

--- src/sudoku.go
+++ src/sudoku.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type sudoku struct {
 	Rows    int
@@ -33,15 +38,27 @@ func printBoard(SudokuBoard *sudoku) {
 	}
 }
 
-func loadBoard(SudokuBoard *sudoku) {
+func loadBoard(SudokuBoard *sudoku, filename string) {
+	file, err := os.ReadFile("./puzzles/" + filename)
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+
+	arrayPosition := 0
+	cleanedFile := strings.ReplaceAll(string(file), "\n", "")
+	data := strings.Split(cleanedFile, ",")
+
 	for i := 0; i < SudokuBoard.Rows; i++ {
 		for j := 0; j < SudokuBoard.Columns; j++ {
-			if SudokuBoard.Board[i][j] == 0 {
-				fmt.Print(".")
-			} else {
-				fmt.Printf("[%v]", SudokuBoard.Board[i][j])
+			value, err := strconv.Atoi(data[arrayPosition])
+			if err != nil {
+				arrayPosition++
+				continue
 			}
+
+			SudokuBoard.Board[i][j] = value
+			arrayPosition++
 		}
-		fmt.Print("\n")
 	}
 }
