51576622871da9cd13b7175aeed4d2e9cd2598f6
timmPrice <timmPrice6@gmail.com>
2025-01-13 16:09:30 -0500

    can now check if a completed puzzle rows and colums are finished

--- src/main.go
+++ src/main.go
@@ -1,8 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	Board := makeBoard(9, 9)
+	loadBoard(Board, "valid-solution.txt")
 	printBoard(Board)
-	loadBoard(Board, "puzzle.txt")
-	printBoard(Board)
+
+	fmt.Println("")
+
+	checkAllColumns(Board)
+	checkAllRows(Board)
 }
--- src/sudoku.go
+++ src/sudoku.go
@@ -25,15 +25,54 @@ func makeBoard(rows int, columns int) *sudoku {
 	}
 }
 
+// returns false if the Row is invalid and true if it is valid
+func checkAllColumns(SudokuBoard *sudoku) bool {
+	// potential speedup by only checking finished lines or specific lines
+	for i := 0; i < SudokuBoard.Columns; i++ {
+		for j := 0; j < SudokuBoard.Rows; j++ {
+			for k := j + 1; k < SudokuBoard.Rows; k++ {
+				if SudokuBoard.Board[j][i] == 0 {
+					continue
+				}
+
+				if SudokuBoard.Board[j][i] == SudokuBoard.Board[k][i] {
+					fmt.Printf("Column %v is invalid\n", i)
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func checkAllRows(SudokuBoard *sudoku) bool {
+	for i := 0; i < SudokuBoard.Rows; i++ {
+		for j := 0; j < SudokuBoard.Columns; j++ {
+			for k := j + 1; k < SudokuBoard.Columns; k++ {
+				if SudokuBoard.Board[i][j] == 0 {
+					continue
+				}
+
+				if SudokuBoard.Board[i][j] == SudokuBoard.Board[i][k] {
+					fmt.Printf("Row %v is invalid\n", i)
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
 func printBoard(SudokuBoard *sudoku) {
 	for i := 0; i < SudokuBoard.Rows; i++ {
 		for j := 0; j < SudokuBoard.Columns; j++ {
 			if SudokuBoard.Board[i][j] == 0 {
-				fmt.Print(".")
+				fmt.Print("[.]")
 			} else {
 				fmt.Printf("[%v]", SudokuBoard.Board[i][j])
 			}
 		}
+
 		fmt.Print("\n")
 	}
 }
